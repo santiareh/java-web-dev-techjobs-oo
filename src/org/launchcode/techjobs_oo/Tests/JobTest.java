e1d56e1eebe9535bee9a780d6d3f04056ef8efcf
santi <Dragonballz95>
2021-02-09 17:59:50 -0600

    Step 4 Started

--- src/org/launchcode/techjobs_oo/Tests/JobTest.java
+++ src/org/launchcode/techjobs_oo/Tests/JobTest.java
@@ -1,9 +1,13 @@
 package org.launchcode.techjobs_oo.Tests;
 
+import org.launchcode.techjobs_oo.Job;
+
 public class JobTest {
 
     @Test
     public void testSettingJobId(){
-
+        Job Test = new Job();
+        Job Test2 = new Job();
+        assertEquals(Test.getId(), Test2.getId(), 1);
     }
 }
