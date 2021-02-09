cf7f16501c8354008c287c706f4ff78d505b2ae1
santi <Dragonballz95>
2021-02-09 17:31:17 -0600

    Step 4 Started

--- src/org/launchcode/techjobs_oo/Location.java
+++ src/org/launchcode/techjobs_oo/Location.java
@@ -14,7 +14,7 @@ public class Location {
 
     // TODO: Add a constructor that takes a string as a parameter and assigns it to the 'value' field. The
     //  constructor should also call the empty constructor in order to initialize the 'id' field.
-    public Test (String newValue){
+    public void Test (String newValue){
         value = newValue;
         this();
     }
